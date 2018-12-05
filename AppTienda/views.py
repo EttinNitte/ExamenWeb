5e79e9a1e647dd8c44aca1b54f9ad0b474551cc0
Eduardo Cea <edu.cea@alumnos.duoc.cl>
2018-12-05 20:47:37 -0300

    urls y vistas

--- AppTienda/views.py
+++ AppTienda/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
+import requests
 from django.shortcuts import render
+from  . import models
 
-# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def registroVenta(request):
+    return render(request,'registroVenta.html')
