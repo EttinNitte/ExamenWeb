97ad0df8875f8d6e93640fbf5c5bff515419cffe
Samuel <s.devia@alumnos.duoc.cl>
2018-12-05 22:35:47 -0300

    modelos vistas y formularios

--- AppTienda/models.py
+++ AppTienda/models.py
@@ -6,6 +6,9 @@ class Vendedor(models.Model):
     contraseña = models.CharField(max_length=30)
     nombre = models.CharField(max_length=100)
     rut = models.CharField(max_length=12)
+    
+    def __str__(self):
+        return self.nombre
 
 class Venta(models.Model):
     vendedor = models.ForeignKey('AppTienda.Vendedor', on_delete=models.CASCADE)
@@ -15,17 +18,29 @@ class Venta(models.Model):
     sucursal = models.ForeignKey('AppTienda.Sucursal', on_delete=models.CASCADE)
     comentario = models.TextField()
 
+    def __str__(self):
+        return 'Venta: '+self.vendedor+' '+self.producto
+
 class Oferta(models.Model):
     producto = models.ForeignKey('AppTienda.Producto', on_delete=models.CASCADE)
     precio = models.IntegerField(max_length=10)
     descuento = models.IntegerField()
+    
+    def __str__(self):
+        return self.producto +' - '+ self.descuento+'%'
 
 class Sucursal(models.Model):
     nombre = models.CharField(max_length=200)
     direccion = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nombre
 
 class Producto (models.Model):
     nombre = models.CharField(max_length=200)
     precio = models.IntegerField()
     descripcion = models.TextField()
     Sucursal = models.ManyToManyField('AppTienda.Sucursal')
+    
+    def __str__(self):
+        return self.nombre
