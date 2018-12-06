a38507b995b3fa1fc7fa135bed8f316cb5d2f9e0
Samuel <s.devia@alumnos.duoc.cl>
2018-12-06 14:59:37 -0300

    final

--- AppTienda/forms.py
+++ AppTienda/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Vendedor,Venta
 
 class LoginForm(forms.ModelForm):
-
+    contraseña = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = Vendedor
         fields = ('usuario', 'contraseña',)
