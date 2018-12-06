824680f18181216e3020c15b0ee96d6dd7b5f7d3
Eduardo Cea <edu.cea@alumnos.duoc.cl>
2018-12-06 03:26:46 -0300

    paginas nuevas

--- AppTienda/urls.py
+++ AppTienda/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
     path('login', views.login, name='login'),
     path('registro_venta', views.registroVenta, name='registro_venta'),
+    path('productos_tienda', views.productosTienda, name='productos_tienda'),
+    path('ofertas_tienda', views.ofertasTienda, name='ofertas_tienda'),
+    path('ventas_realizadas', views.ventasRealizadas, name='ventas_realizadas'),
 ]
--- AppTienda/views.py
+++ AppTienda/views.py
@@ -5,18 +5,40 @@ from django.shortcuts import render
 from  . import models
 from .forms import VentaForm,LoginForm
 from .models import Producto
+from .models import Oferta
+from .models import Venta
 
 def index(request):
     response = requests.get('https://mindicador.cl/api')
     data = response.json()
+    return render(request,'index.html',{'dolar': data['dolar']['valor']})
+    
+    
+def productosTienda(request):
+    response = requests.get('https://mindicador.cl/api')
+    data = response.json()
     productos = Producto.objects.all()
-    return render(request,'index.html',{'dolar': data['dolar']['valor'],'productos': productos})
+    return render(request,'productosTienda.html',{'dolar': data['dolar']['valor'],'productos': productos})
+
+def ofertasTienda(request):
+    response = requests.get('https://mindicador.cl/api')
+    data = response.json()
+    ofertas = Oferta.objects.all()
+    return render(request,'ofertasTienda.html',{'dolar': data['dolar']['valor'],'ofertas': ofertas})
+
+def ventasRealizadas(request):
+    response = requests.get('https://mindicador.cl/api')
+    data = response.json()
+    ventas = Venta.objects.all()
+    return render(request,'ventasRealizadas.html',{'dolar': data['dolar']['valor'],'ventas': ventas})
 
 def login(request):
     form = LoginForm()
     return render(request,'login.html', {'form': form})
 
 def registroVenta(request):
+    response = requests.get('https://mindicador.cl/api')
+    data = response.json()
     form = VentaForm()
     if request.method == "POST":
         form = VentaForm(request.POST)
@@ -26,4 +48,4 @@ def registroVenta(request):
             return redirect('index')
     else:
         form = VentaForm()
-    return render(request, 'registroVenta.html', {'form': form})
+    return render(request, 'registroVenta.html', {'form': form,'dolar': data['dolar']['valor']})
