35c35ecedf43d98d37948476ecdd1122efeab1b1
Samuel <s.devia@alumnos.duoc.cl>
2018-12-06 12:35:34 -0300

    login

--- AppTienda/views.py
+++ AppTienda/views.py
@@ -7,33 +7,64 @@ from .forms import VentaForm,LoginForm
 from .models import Producto
 from .models import Oferta
 from .models import Venta
+from .models import Vendedor
+
+usuarioLogeado = 0
 
 def index(request):
     response = requests.get('https://mindicador.cl/api')
     data = response.json()
-    return render(request,'index.html',{'dolar': data['dolar']['valor']})
+    global usuarioLogeado
+    if usuarioLogeado == 0:
+        return redirect('login')
+    return render(request,'index.html',{'dolar': data['dolar']['valor'],'usuario': usuarioLogeado})
     
     
 def productosTienda(request):
     response = requests.get('https://mindicador.cl/api')
     data = response.json()
-    productos = Producto.objects.all()
+    global usuarioLogeado
+    if usuarioLogeado == 0:
+        return redirect('login')
+    print(usuarioLogeado)
+    #print(usuarioLogeado.Tienda)
+    productos = Producto.objects.all()#.filter(Tienda=usuarioLogeado.Tienda)
     return render(request,'productosTienda.html',{'dolar': data['dolar']['valor'],'productos': productos})
 
 def ofertasTienda(request):
     response = requests.get('https://mindicador.cl/api')
     data = response.json()
+    global usuarioLogeado
+    if usuarioLogeado == 0:
+        return redirect('login')
     ofertas = Oferta.objects.all()
     return render(request,'ofertasTienda.html',{'dolar': data['dolar']['valor'],'ofertas': ofertas})
 
 def ventasRealizadas(request):
     response = requests.get('https://mindicador.cl/api')
     data = response.json()
-    ventas = Venta.objects.all()
+    global usuarioLogeado
+    if usuarioLogeado == 0:
+        return redirect('login')
+    ventas = Venta.objects.all().filter(vendedor = usuarioLogeado)
     return render(request,'ventasRealizadas.html',{'dolar': data['dolar']['valor'],'ventas': ventas})
 
 def login(request):
+    response = requests.get('https://mindicador.cl/api')
+    dataApi = response.json()
     form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            print("FORMA VALIDA")
+            data=form.cleaned_data
+            usuarioValidado = Vendedor.objects.all().filter(usuario=data.get("usuario"),contraseña=data.get("contraseña"))
+            if usuarioValidado.count() == 1:
+                global usuarioLogeado
+                usuarioLogeado = usuarioValidado
+                return redirect('index')
+    else:
+        form = LoginForm()
     return render(request,'login.html', {'form': form})
 
 def registroVenta(request):
